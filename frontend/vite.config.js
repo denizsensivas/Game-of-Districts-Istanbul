16334128155abc4662585e3f3b94551fac728e69
Zeynep Deniz Sensivas <denizsensivas@gmail.com>
2026-06-14 18:53:46 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   preview: {
     allowedHosts: [
-      'laudable-ambition-production-63fa.up.railway.app'
+      'laudable-ambition-production-8b82.up.railway.app'
     ]
   }
 })
